a76f38160d1dda929c966f66308fc2f77d55f6a6
PatrykWrzalek <wrzalek.patryk@gmail.com>
2024-10-24 12:07:22 +0200

    SOFTAP MODE
    Initializes ESP8266 in Soft AP Mode to give clients the opportunity to get connected with AP on ESP.

--- main/main.c
+++ main/main.c
@@ -13,20 +13,69 @@
 #define TAG "ESP8266"
 #define HOSTNAME "ESP_Iot"
 #define AP_SSID "ESP_IOT"
-#define AP_PASS "ESP8266"
-#define MAX_STA_CON_TO_AP 4
+#define AP_PASS "MyESP8266"
+#define MAX_STA_CON_TO_AP 5
 
 void gpio_init(void);
+void esp_AP_init(void);
 
-void esp_AP_init(void *arg);
+void system_init(void *arg);
 void workStatus(void *arg);
 
+// Funkcja callback do obsługi zdarzeń WiFi
+static void wifi_event_handler(void *arg, esp_event_base_t event_base,
+                               int32_t event_id, void *event_data)
+{
+    switch (event_id) // Sprawdź rodzaj zdarzenia
+    {
+    case WIFI_EVENT_AP_STACONNECTED: // Gdy nowe urządzenie (klient) połączy się z ESP działającym jako AP
+    {
+        wifi_event_ap_staconnected_t *event = (wifi_event_ap_staconnected_t *)event_data;  // Rzutowanie danych
+        ESP_LOGI(TAG, "station " MACSTR " join, AID=%d", MAC2STR(event->mac), event->aid); // Wypisz adres MAC klienta oraz jego AID (identyfikator)
+        break;
+    }
+    case WIFI_EVENT_AP_STADISCONNECTED: // Gdy urządzenie (klient) rozłączy się z ESP działającym jako AP
+    {
+        wifi_event_ap_stadisconnected_t *event = (wifi_event_ap_stadisconnected_t *)event_data; // Rzutowanie danych
+        ESP_LOGI(TAG, "station " MACSTR " leave, AID=%d", MAC2STR(event->mac), event->aid);     // Wypisz adres MAC klienta oraz jego AID
+        break;
+    }
+    default:
+        break; // Jeżeli zdarzenie nie jest rozpoznane, nic nie rób
+    }
+}
+
+/******************************************************************************
+ * FunctionName : app_main
+ * Description  : entry of app application, init app function here.
+ * Users can use tasks with priorities from 1 to 9
+ * (priority of the freeRTOS timer is 2).
+ * Parameters   : none
+ * Returns      : none
+ *******************************************************************************/
 void app_main(void)
+{
+    xTaskCreate(system_init, "Init_system", 2048, NULL, 9, NULL);
+
+    xTaskCreate(workStatus, "Status", 1024, NULL, 1, NULL);
+}
+
+/******************************************************************************
+ * FunctionName : system_init
+ * Description  : main initializes task.
+ * Parameters   : none
+ * Returns      : none
+ *******************************************************************************/
+void system_init(void *arg)
 {
     gpio_init();
 
-    xTaskCreate(esp_AP_init, "ap_init", 2048, NULL, 5, NULL);
-    xTaskCreate(workStatus, "Status", 1024, NULL, 1, NULL);
+    vTaskDelay(5000 / portTICK_RATE_MS);
+    esp_AP_init();
+
+    uint16_t siz_dat = uxTaskGetStackHighWaterMark(NULL); // Info for debug
+    ESP_LOGI(TAG, "Task2 stack: %d\r\n", siz_dat);
+    vTaskDelete(NULL);
 }
 
 /******************************************************************************
@@ -70,16 +119,15 @@ void gpio_init(void)
  * Parameters   : none
  * Returns      : none
  *******************************************************************************/
-void esp_AP_init(void *arg)
+void esp_AP_init(void)
 {
-    vTaskDelay(1000 / portTICK_RATE_MS);
-
     ESP_ERROR_CHECK(nvs_flash_init());                // Inicjalizacja pamięci NVS (Non-Volatile Storage)
     esp_netif_init();                                 // Inicjalizacja adaptera TCP/IP
     ESP_ERROR_CHECK(esp_event_loop_create_default()); // Tworzenie domyślnej pętli zdarzeń
 
-    wifi_init_config_t wifi_config = WIFI_INIT_CONFIG_DEFAULT(); // Konfiguracja Wi-Fi z domyślnymi ustawieniami
-    ESP_ERROR_CHECK(esp_wifi_init(&wifi_config));                // Inicjalizacja Wi-Fi
+    wifi_init_config_t wifi_config = WIFI_INIT_CONFIG_DEFAULT();                                          // Konfiguracja Wi-Fi z domyślnymi ustawieniami
+    ESP_ERROR_CHECK(esp_wifi_init(&wifi_config));                                                         // Inicjalizacja Wi-Fi
+    ESP_ERROR_CHECK(esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &wifi_event_handler, NULL)); // Rejestarcja funkcja callback do obsługi zdarzeń WiFi
 
     wifi_config_t esp_ap_config;                         // Struktura do przechowywania konfiguracji AP
     sprintf((char *)esp_ap_config.ap.ssid, AP_SSID);     // Ustawienie SSID AP
@@ -88,15 +136,15 @@ void esp_AP_init(void *arg)
     esp_ap_config.ap.authmode = WIFI_AUTH_WPA_WPA2_PSK;  // Ustawiamy tryb uwierzytelniania na WPA/WPA2, jeśli hasło jest ustawione
     esp_ap_config.ap.ssid_hidden = 0;                    // Ustalamy czy sieć ma być widoczna
     esp_ap_config.ap.max_connection = MAX_STA_CON_TO_AP; // Ustalamy maksymalną liczbę połączeń
-    if (strlen(AP_PASS) == 0)                            // Jeśli brak hasła
+    if ((8 > strlen(AP_PASS)) || (strlen(AP_PASS) > 63)) // Niepoprawna ilość znaków dla hasła (wymóg standardu WPA/WPA2-PSK od 8 do 63 znaków)
     {
-        esp_ap_config.ap.authmode = WIFI_AUTH_OPEN; // Ustawiamy tryb otwarty
+        ESP_LOGI(TAG, "Wrong AP_PASS len: %d\r\n", strlen(AP_PASS)); // Info for debug
+        esp_ap_config.ap.authmode = WIFI_AUTH_OPEN;                  // Ustawiamy tryb otwarty
     }
 
     ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_AP));                     // Ustawienie trybu Wi-Fi na AP (Access Point)
     ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_AP, &esp_ap_config)); // Ustawienie konfiguracji dla AP
     ESP_ERROR_CHECK(esp_wifi_start());                                    // Uruchomienie Wi-Fi
 
-    ESP_LOGI(TAG, "esp_AP_init finished. SSID:%s password:%s", AP_SSID, AP_PASS); // Info
-    vTaskDelete(NULL);
+    ESP_LOGI(TAG, "esp_AP_init finished. SSID:%s password:%s\r\n", AP_SSID, AP_PASS); // Info for debug
 }
